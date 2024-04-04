4592215fa2044a5728e184af9526f14ddd5f7482
Amir-Thp <code2time@gmail.com>
2024-04-04 16:19:02 +0330

    create basic  Routing project

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 //   use Express
-const { error } = require('console');
-const express = require('express');
+const express = require("express");
 const app = express();
-
-
 
 //  listen default on port (3000)
 app.listen(3000);
 
 // Reset viwe Engine
 
-app.set('view engine' , 'ejs');
-app.set('views' , 'pages');
+app.set("view engine", "ejs");
+app.set("views", "pages");
 
-// set Routing 
-app.get('/' , (req , res) => {
-    res.render('index');
-})
+// set Routing
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
 
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
 
+app.get("/blogs", (req, res) => {
+  res.render("blogs", { title: "Blogs" });
+});
+
+app.get("/blogs/create-blog", (req, res) => {
+  res.render("createblogs", { title: "Create Blogs" });
+});
+
+app.use((req, res) => {
+  res.render("404", { title: "404" });
+});
