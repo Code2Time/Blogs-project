706dfd055471950b8d1dc0ce8d9196b92e9df110
Amir-Thp <code2time@gmail.com>
2024-04-09 13:20:26 +0330

    update styles

--- app.js
+++ app.js
@@ -51,10 +51,6 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-app.get("/blogs", (req, res) => {
-  res.render("blogs", { title: "Blogs" });
-});
-
 app.get("/blogs/create-blog", (req, res) => {
   res.render("createblogs", { title: "Create Blogs" });
 });
